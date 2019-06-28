70b4c75bfe6404cb4a233d20528008185c277728
ECKRate <ECKRate@gmail.com>
2019-06-28 22:25:20 +0300

    migrate to service

--- app/src/main/java/com/codegemz/kotlinx/lesson4/MainActivity.kt
+++ app/src/main/java/com/codegemz/kotlinx/lesson4/MainActivity.kt
@@ -1,31 +1,85 @@
 package com.codegemz.kotlinx.lesson4
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
+import android.graphics.Color
 import android.os.Bundle
+import android.os.IBinder
 import android.view.Gravity
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view: MainView
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            if (service is MyService.LocalBinder) {
+                service.onPlayingStarted = {
+                    view.updatePlayButton(true)
+                }
+                service.onPlayingStopped = {
+                    view.updatePlayButton(false)
+                }
+            }
+            view.updatePlayButton(false)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MainView().setContentView(this)
+        view = MainView()
+        view.setContentView(this)
+
+        // Bind service for receiving callbacks
+        val intent = Intent(this, MyService::class.java)
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Stop when close the app
+        MyService.stopService(this)
+        unbindService(serviceConnection)
     }
 }
 
 class MainView : AnkoComponent<MainActivity> {
+    private lateinit var playButton: Button
+
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         relativeLayout {
-            button("Play music") {
-                onClick {
-                    val intent = Intent(ctx, IntentService::class.java)
-                    intent.setAction(ACTION_PLAY_MUSIC);
-                    ctx.startService(intent)
-                }
-            }.lparams() {
+            playButton = button("Waiting for service..") {
+
+            }.lparams {
                 gravity = Gravity.CENTER_HORIZONTAL
                 topMargin = dip(70)
+            }
+        }
+    }
+
+    internal fun updatePlayButton(isPlaying: Boolean) {
+        with(playButton) {
+            if (isPlaying) {
+                text = resources.getString(R.string.stop_music)
+                setBackgroundColor(Color.CYAN)
+                onClick {
+                    MyService.stop(context)
+                }
+            } else {
+                text = resources.getString(R.string.play_music)
+                setBackgroundColor(Color.GREEN)
+                onClick {
+                    MyService.play(context)
+                }
             }
         }
     }
---
+++ app/src/main/java/com/codegemz/kotlinx/lesson4/MyService.kt
@@ -0,0 +1,96 @@
+package com.codegemz.kotlinx.lesson4
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+
+const val ACTION_PLAY_MUSIC = "com.codegemz.kotlinx.lesson4.action.PLAY_MUSIC"
+const val ACTION_STOP_MUSIC = "com.codegemz.kotlinx.lesson4.action.STOP_MUSIC"
+const val ACTION_STOP_SERVICE = "com.codegemz.kotlinx.lesson4.action.STOP_SERVICE"
+
+/**
+ * A [Service] is an application component that can perform long-running operations
+ * in the background, and it doesn't provide a user interface.
+ */
+class MyService : Service() {
+    private lateinit var mediaPlayer: MediaPlayer
+    private var binder: LocalBinder = LocalBinder()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return binder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // creating player instance for playing music
+        mediaPlayer = MediaPlayer.create(this, R.raw.bell)
+        mediaPlayer.setOnCompletionListener {
+            binder.onPlayingStopped?.invoke()
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        onHandleIntent(intent)
+        return START_STICKY
+    }
+
+    fun onHandleIntent(intent: Intent?) {
+        when (intent?.action) {
+            ACTION_PLAY_MUSIC -> {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.stop()
+                    mediaPlayer.prepare()
+                }
+                mediaPlayer.start()
+                binder.onPlayingStarted?.invoke()
+            }
+            ACTION_STOP_MUSIC -> {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.stop()
+                    mediaPlayer.prepare()
+                    binder.onPlayingStopped?.invoke()
+                }
+            }
+            ACTION_STOP_SERVICE -> {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.stop()
+                }
+                stopSelf()
+            }
+        }
+    }
+
+    companion object {
+        // Start play music
+        fun play(context: Context) {
+            with(Intent(context, MyService::class.java)) {
+                action = ACTION_PLAY_MUSIC
+                context.startService(this)
+            }
+        }
+
+        // Stop music
+        fun stop(context: Context) {
+            with(Intent(context, MyService::class.java)) {
+                action = ACTION_STOP_MUSIC
+                context.startService(this)
+            }
+        }
+
+        // Stop service
+        fun stopService(context: Context) {
+            val intent = Intent(context, MyService::class.java)
+            intent.action = ACTION_STOP_SERVICE
+            context.startService(intent)
+        }
+    }
+
+    // Binder class implementation. Used to connection
+    inner class LocalBinder : Binder() {
+        var onPlayingStarted: (() -> Unit)? = null
+        var onPlayingStopped: (() -> Unit)? = null
+    }
+}
